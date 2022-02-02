35605a8309b5e762181877d00e2b9ad5820da84d
Alejandro Hernández <alejandroohr@gmail.com>
2022-02-01 19:46:51 -0600

    Adding components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
 
+import Header from './components/Header/Header'
+import List from './components/List/List'
+import Map from './components/Map/Map'
+
 const App = () => {
   return (
     <div>
       Hello World!
+      <Header />
+      <List />
+      <Map />
     </div>
   )
 }
