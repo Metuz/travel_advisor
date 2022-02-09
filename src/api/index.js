3341f530e8a1828f8c49976ccbe47423fe88b490
Alejandro Hernández <alejandroohr@gmail.com>
2022-02-09 16:49:14 -0600

    Adding function on type and ratingfilters

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary';
+export const getPlacesData = async (type, sw, ne) => {
+  const url = `https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`
 
-export const getPlacesData = async (sw, ne) => {
   try {
-    const { data: { data } } = await axios.get(URL, {
+    const { data: { data } } = await axios.get(url, {
       params: {
         bl_latitude: sw.lat,
         tr_latitude: ne.lat,
